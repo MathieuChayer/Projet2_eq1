1be6e83aac2ef6965742af08ee09c5b4bfbf7036
andrea-s1 <80648068+andrea-s1@users.noreply.github.com>
2021-03-31 19:43:56 -0400

    écriture de la fonction permettant de convertir le vecteur du signal original - uint32 - en vecteur uint16 qui soit bien réparti dans l'écran (normalisation) : conversionVecteur écriture de la fonction pour afficher le signal : drawGraph (mais n'est pas encore fonctionnelle)

---
+++ WorkspaceProjet2Eq1/Projet2Eq1.cydsn/interface.h
@@ -0,0 +1,21 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include "GUI.h"
+#include "pervasive_eink_hardware_driver.h"
+#include "cy_eink_library.h"
+#include "LCDConf.h"
+#include <stdlib.h>
+#include <math.h>
+
+
+/* [] END OF FILE */
