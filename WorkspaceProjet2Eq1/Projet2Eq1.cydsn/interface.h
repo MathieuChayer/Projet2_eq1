4c748b5f92b8f374cfb08df1b6462a3b4aa6a769
andrea-s1 <80648068+andrea-s1@users.noreply.github.com>
2021-04-07 18:11:05 -0400

    affichage fonctionnel (courbe, type et rectangles des valeurs) Il reste à trouver comment inclure le updateParameters dans la fonction d'affichage pour qu'on puisse voir des valeurs.

--- WorkspaceProjet2Eq1/Projet2Eq1.cydsn/interface.h
+++ WorkspaceProjet2Eq1/Projet2Eq1.cydsn/interface.h
@@ -16,6 +16,17 @@
 #include "LCDConf.h"
 #include <stdlib.h>
 #include <math.h>
+#include "FreeRTOS.h"
+#include "task.h"
+//#include "touch_task.h"
+//#include "display_task.h"
+//#include "uart_debug.h" 
 
+
+void conversionVecteur(uint32_t* vecteur,  int longueur, uint16_t* bonVecteur);
+void drawGraph(uint16_t *vecteur200elements);
+void updateParameters(int param1, int param2);
+void interface(void *pvParameters);
+void drawAffichageCourbe(void);
 
 /* [] END OF FILE */
